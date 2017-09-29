c57f04ac641076c8ff48d12f162aa09b1772cfa1
Adarsh <adarshan_naiynar@icloud.com>
2017-09-29 15:48:11 +0530

    Added sematic ui

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'semantic-ui-react';
 
 require('styles/app.scss');
 
 const App = props => (
   <div>
     <p id="value">Value: {props.value}</p>
-    <button id="increment_button" onClick={props.increment} value="Increment">Increment</button>
-    <button id="decrement_button" onClick={props.decrement} value="Decrement">Decrement</button>
+    <Button id="increment_button" onClick={props.increment} value="Increment">Increment</Button>
+    <Button id="decrement_button" onClick={props.decrement} value="Decrement">Decrement</Button>
   </div>
 );
 
